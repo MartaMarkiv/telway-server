45737db826e59a1d6f0cf5786f3747f8051db45f
Marta Markiv <mpat@keplercode.com>
2025-06-25 13:47:00 +0300

    Added webhook controller

--- controllers/paymnet/webhook.js
+++ controllers/paymnet/webhook.js
@@ -1,13 +1,22 @@
 // const stripe = require("../../lib/stripe");
+const UserModel = require("../../models/User");
 
 module.exports = async(req, res) => {
   try {
     console.log("WEBHOOK");
 
     const {data} = req.body;
-    console.log(data);
-    console.log("-------------------------------------------------------------");
     console.log(data.object);
+
+    const {amount, metadata: {userId}} = data.object;
+
+    const user = await UserModel.findById(userId);
+
+    const updatedBalance = user.balance | 0 + amount / 1000;
+
+    console.log("updatedBalance: ", updatedBalance);
+
+    await UserModel.updateBalance(userId,updatedBalance);
 
     return res.status(200).json({ message: "Success" });
   } catch (error) {
