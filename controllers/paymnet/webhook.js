4924b3e0b414e0f87d3de6d05d1ba8e4b231f9d3
Marta Markiv <mpat@keplercode.com>
2025-06-25 11:19:12 +0300

    Added console for webhook testing'

--- controllers/paymnet/webhook.js
+++ controllers/paymnet/webhook.js
@@ -3,9 +3,11 @@
 module.exports = async(req, res) => {
   try {
     console.log("WEBHOOK");
-    console.log(req.body);
 
-
+    const {data} = req.body;
+    console.log(data);
+    console.log("-------------------------------------------------------------");
+    console.log(data.object);
 
     return res.status(200).json({ message: "Success" });
   } catch (error) {
