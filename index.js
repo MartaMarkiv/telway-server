b01bfa959c3e929500d3f1a14827d0351c62bf31
Marta Markiv <mpat@keplercode.com>
2025-07-14 14:23:18 +0300

    Updated webhook router

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const corsOptions = {
 console.log("CORS allowed origins:", corsOptions.origin);
 
 // app.options("*", cors(corsOptions));
-app.use("/payment/webhook", webhookController);
+app.use("/payment/webhook", express.raw({ type: 'application/json' }), webhookController);
 app.use(cors(corsOptions));
 app.use(bodyParser.json());
 app.use(cookieParser());
