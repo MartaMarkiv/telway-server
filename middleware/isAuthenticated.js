0fc2c4b182995c66374a3e6dae3c485931989466
Marta Markiv <mpat@keplercode.com>
2025-10-02 14:54:55 +0300

    Added console for test

--- middleware/isAuthenticated.js
+++ middleware/isAuthenticated.js
@@ -4,7 +4,10 @@ const auth = (req, res, next) => {
   const authHeader = req.headers["authorization"];
   const token = authHeader && authHeader.split(" ")[1];
 
-  if (!token) return res.sendStatus(401);
+  if (!token) {
+    console.log("NOT TOKEN RETURN");
+    return res.sendStatus(401);
+  }
 
   jwt.verify(token, process.env.ACCESS_SECRET, (err, user) => {
     if (err) return res.sendStatus(403);
