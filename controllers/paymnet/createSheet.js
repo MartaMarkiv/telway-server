24a5dcdb234c14cdee6a8e4a9bea79ebc0636c3f
Marta Markiv <mpat@keplercode.com>
2025-10-02 15:09:49 +0300

    Removed version

--- controllers/paymnet/createSheet.js
+++ controllers/paymnet/createSheet.js
@@ -10,7 +10,7 @@ module.exports = async(req, res) => {
 
 
     const customer = await stripe.customers.create();
-    const ephemeralKey = await stripe.ephemeralKeys.create( {customer: customer.id},{apiVersion:"2025-01-27"});
+    const ephemeralKey = await stripe.ephemeralKeys.create( {customer: customer.id});
 
     const paymentIntent = await stripe.paymentIntents.create({
       amount: sendAmount,
