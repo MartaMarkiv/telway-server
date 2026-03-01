358ddba2e3f3b76e898214c48134f51fe869b2e1
Marta Markiv <markiv.marta.b@gmail.com>
2026-03-01 23:22:54 +0200

    Added Activity model

---
+++ models/Activity.js
@@ -0,0 +1,22 @@
+const mongoose = require("../lib/mongoose");
+const Schema = mongoose.Schema;
+
+const Activity = Schema({
+  name: String,
+  duration: Number,
+  price: Number,
+  type: String,
+  user: { type: Schema.Types.ObjectId, ref: "User" }
+},
+{
+  timestamps: true
+});
+
+const ActivityModel = mongoose.model("Activity", Activity);
+
+module.exports = {
+  model: ActivityModel,
+  create:  (activity) => new ActivityModel(activity).save(),
+  findByUser:  (userId) => ActivityModel.findOne({user: userId}),
+  findById:  (_id) => ActivityModel.findOne({_id}),
+}
