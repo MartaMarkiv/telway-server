cfb26a286ba7aa62fd953131b321a4ed5ac1a3d2
Marta Markiv <markiv.marta.b@gmail.com>
2026-06-24 13:03:47 +0300

    Updated port

--- controllers/sip/getCredential.js
+++ controllers/sip/getCredential.js
@@ -2,8 +2,8 @@ module.exports = async(req, res) => {
   try {
 
     console.log("get credential request");
-    // const wsUrl = process.env.SIP_WS_URL || 'wss://62.171.166.141:5060/wss';
-    const wsUrl = 'ws://62.171.166.141:5066/ws'; // або інший порт розробки
+    const wsUrl = 'ws://62.171.166.141:5060/ws';
+    // const wsUrl = 'ws://62.171.166.141:5066/ws';
 
     return res.status(200).json({
       sipUri: 'client1',
